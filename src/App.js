12684fde948d951e67f4b943043643a3efdb4b04
allsgit <allanseu@gmail.com>
2022-11-22 03:28:33 +0100

    add correct icon for clear

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 
 function App() {
   const key = `392295a71823ede3e5a56a6b97ff2bac`;
-
   const [data, setData] = useState({});
   const [location, setLocation] = useState('lyon');
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=fr&appid=${key}&units=metric`;
@@ -23,43 +22,54 @@ function App() {
     data.weather ? data.weather.map((el) => el.icon) : ''
   }@2x.png`;
 
-  function colorSwitch () {
+  function colorSwitch() {
     if (data.main.temp >= 20) {
-      return "temp-jaune"
+      return 'temp-jaune';
     } else if (data.main.temp < 20 && data.main.temp > 10) {
-      return "temp"
-    } else if (data.main.temp < 10 ){
-      return "temp-blue"
+      return 'temp';
+    } else if (data.main.temp < 10) {
+      return 'temp-blue';
     }
   }
-  
 
   return (
     <div className="App">
-  
       <div className="weather-container">
-      <div className="search-div">
-        <input
-          type="text"
-          value={location}
-          onChange={(event) => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder="Entrer la ville"
-          className="search-input"
-        />
-      </div>
+        <div className="search-div">
+          <input
+            type="text"
+            value={location}
+            onChange={(event) =>
+              setLocation(event.target.value)
+            }
+            onKeyPress={searchLocation}
+            placeholder="Entrer la ville"
+            className="search-input"
+          />
+        </div>
         <div className="top">
           {' '}
           <div className="location">
             <p>
               {data.name ? (
-                <p>{data.name.replace('Arrondissement de', ' ')}</p>
+                <p>
+                  {data.name.replace(
+                    'Arrondissement de',
+                    ' '
+                  )}
+                </p>
               ) : (
                 ' '
               )}
             </p>
           </div>
-          <div className={data.main ? colorSwitch(data.main.temp) : "temp"}>
+          <div
+            className={
+              data.main
+                ? colorSwitch(data.main.temp)
+                : 'temp'
+            }
+          >
             {data.main ? <p>{data.main.temp} °C</p> : ''}
           </div>
         </div>
@@ -75,13 +85,13 @@ function App() {
                     if (el.main == 'Clouds') {
                       return 'Nuageux';
                     } else if (el.main == 'Clear') {
-                      return 'Ensoleillé';
+                      return 'Dégagé';
                     } else if (el.main == 'Rain') {
                       return 'Pluie';
                     } else if (el.main == 'Thnderstrom') {
                       return 'Orage';
-                    } else if (el.main == "Mist") {
-                      return "Brouillard"
+                    } else if (el.main == 'Mist') {
+                      return 'Brouillard';
                     }
                   })}
                 </p>
@@ -96,14 +106,30 @@ function App() {
           <div className="reel">
             <p>
               Température ressentie :
-              {data.main ? <p>{data.main.feels_like} °C</p> : ' '}
+              {data.main ? (
+                <p>{data.main.feels_like} °C</p>
+              ) : (
+                ' '
+              )}
             </p>
           </div>
           <div className="humidity">
-            <p>Humidité : </p> {data.main ? <p>{data.main.humidity} %</p> : ' '}
+            <p>Humidité : </p>{' '}
+            {data.main ? (
+              <p>{data.main.humidity} %</p>
+            ) : (
+              ' '
+            )}
           </div>
           <div className="wind-speed">
-            <p>Vent : {data.wind ? <p>{data.wind.speed} KM/H </p> : ' '} </p>
+            <p>
+              Vent :{' '}
+              {data.wind ? (
+                <p>{data.wind.speed} KM/H </p>
+              ) : (
+                ' '
+              )}{' '}
+            </p>
           </div>
         </div>
       </div>
